669df728df033d4c71c965ad4311944c90cf9b0e
melanylibia <elibialany@gmail.com>
2024-09-02 21:02:24 -0400

    feat: update port app

--- src/app.py
+++ src/app.py
@@ -64,4 +64,4 @@ def update_bookmark(id):
     return redirect(url_for('Index'))
 
 if __name__ == '__main__':
-    app.run(port=3000,debug=True)
+    app.run(port=os.getenv('LINK_APP_PORT'),debug=True)
